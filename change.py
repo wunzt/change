3acfaee41e1c9869e5f00b65af3eef59e321622b
wunzt <wunzt@oregonstate.edu>
2022-04-05 21:28:11 -0700

    Assignment 2.c

---
+++ change.py
@@ -0,0 +1,17 @@
+# Author: Thomas Wunz
+# GitHub username: wunzt
+# Date: 4/5/2022
+# Description: Asks the user to input an amount of cents less than a dollar and prints the change required in the
+#   fewest possible coins
+
+print("Please enter an amount in cents less than a dollar.")
+prime = float(input())
+quarters = prime // 25
+dimes = (prime % 25) // 10
+nickels = ((prime - (quarters * 25)) % 10) // 5
+pennies = (prime % 5) // 1
+print("Your change will be:")
+print("Q: ", quarters)
+print("D: ", dimes)
+print("N: ", nickels)
+print("P: ", pennies)
